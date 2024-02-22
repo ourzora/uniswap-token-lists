d3082695fc3b23d3a0f3e8ea6b36431c37cb1b2b
tbtstl <tysonbattistella@gmail.com>
2024-02-22 12:22:02 -0500

    README

--- src/validate.ts
+++ src/validate.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import ajv from 'ajv';
+import addFormats from 'ajv-formats';
 import { TokenList } from 'types';
 
 function run() {
@@ -9,12 +10,13 @@ function run() {
   const schema = JSON.parse(
     fs.readFileSync('src/tokenlist.schema.json', 'utf-8')
   );
-  const validate = new ajv().compile(schema);
+  const validator = new ajv();
+  addFormats(validator);
 
-  const valid = validate(list);
+  const valid = validator.validate(schema, list);
 
   if (!valid) {
-    console.error(validate.errors);
+    console.error(validator.errors);
     process.exit(1);
   } else {
     console.log('Tokenlist is valid!');
